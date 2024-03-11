61a67f1675e4d6231a6cd12b7db4f6664f525890
Orel Chalfon <orelchalfon12@gmail.com>
2024-03-11 22:28:23 +0200

    adding Pages Folder{Home,About etc..}

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,6 @@
+const Home = () => {
+  return <div className="min-h-[35%] w-full bg-red-400">Home</div>;
+};
+
+export default Home;
+
---
+++ src/Pages/index.js
@@ -0,0 +1,6 @@
+import About from './About';
+import Contact from './Contact';
+import Home from './Home';
+import NotFound from './NotFound';
+import Projects from './Projects';
+export { About, Contact, Home, NotFound, Projects };
