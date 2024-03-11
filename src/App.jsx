15796b47386c91aee5d94e6c51ac6a95c46bc8b5
Orel Chalfon <orelchalfon12@gmail.com>
2024-03-11 22:26:49 +0200

    init vite react proj

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import { About, Contact, Home, Projects } from './Pages';
+const App = () => {
+  return (
+    <main
+      className="
+      
+      flex h-svh min-h-svh
+      items-center
+      justify-center
+      bg-black-500 text-white 
+      
+      "
+    >
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
