3ae5c7b4b6d7580af9b03bc6dad2c80f397caa3d
Orel Chalfon <orelchalfon12@gmail.com>
2024-03-13 16:41:40 +0200

    fixing minor conf

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base:"/3d",
+  base:"/3d-portfolio/",
   plugins: [react()],
+  assetsInclude: ['**/*.glb'],
 });
