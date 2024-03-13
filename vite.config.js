0157faf8e329db52d6487a24e0a2fef49da58896
Orel Chalfon <orelchalfon12@gmail.com>
2024-03-13 16:47:47 +0200

    lastconf

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base:"/3d-portfolio/",
+  base:"/3d_portfolio/",
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 });
